26f2878c0ffb4e5d77147b3b15b1888e06126e42
Ved V Joshi <152370268+Blankstar1233@users.noreply.github.com>
2024-10-13 15:52:19 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 // Simple Java Program
-public class HelloWorld {
+public class main {
     public static void main(String[] args) {
         // Prints "Hello, World!" to the console
         System.out.println("Hello, World!");
